cbc61fea4af6a6e6dfe563fe3fe12a45fcaf25de
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-07 14:44:00 +0100

    Refactor HTML structure for improved readability and consistency; adjust JavaScript formatting for better clarity

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Number 1 Assignment
 function enterAddress() {
-    var lowerAddress = input1.value.toLowerCase() 
-newAddress.innerHTML = lowerAddress
-} 
+    var lowerAddress = input1.value.toLowerCase()
+    newAddress.innerHTML = lowerAddress
+}
 
 // Number 3 Assignment
- function newEssay() {
+function newEssay() {
     var essay = essay1.value.toLowerCase().trim()
     var newEssay = essay.split(/\s+/)
     editedEssay.innerHTML = newEssay
- }
+}
 
 
- // Number 7 Assignment
+// Number 7 Assignment
 function checkAnswer() {
     var answer = quiz.value.toLowerCase()
     if (answer === "yes") {
-        result.innerHTML = "Correct!" 
+        result.innerHTML = "Correct!"
     } else {
         result.innerHTML = "Incorrect!"
     }
@@ -24,28 +24,28 @@ function checkAnswer() {
 
 // Number 6 Assignment
 function checkWeather() {
-    var weatherInput = Number(weather.value) 
-    if (weatherInput <=19) {
+    var weatherInput = Number(weather.value)
+    if (weatherInput <= 19) {
         weatherResult.innerHTML = "It's a cold day!"
-    } else if (weatherInput >19 && weather <=28) {
+    } else if (weatherInput > 19 && weather <= 28) {
         weatherResult.innerHTML = "It's a warm day!"
-    } else if( weatherInput >=29) {
+    } else if (weatherInput >= 29) {
         weatherResult.innerHTML = "It's a hot day!"
-    }else {
+    } else {
         weatherResult.innerHTML = "Invalid input!"
     }
-    
+
 }
 
 
 // Number 5 Assignment
 function checkReward() {
-    var rewardInput = Number(reward.value) 
-    if (rewardInput%2 ==1) {
+    var rewardInput = Number(reward.value)
+    if (rewardInput % 2 == 1) {
         rewardResult.innerHTML = "You are not eligible for a reward!"
-    }else {
+    } else {
         rewardResult.innerHTML = "You are eligible for a reward!"
     }
-    
+
 }
 
